61a698965c8dab6c00c009fc00f3f3f0d51af5fd
maratreason <maratreason@mail.ru>
2021-01-30 16:20:52 +0300

    Сделан адаптив

--- js/main.js
+++ js/main.js
@@ -4,9 +4,41 @@ document.addEventListener("DOMContentLoaded", () => {
 	const modalTriggers = document.querySelectorAll(".show-modal");
 	const closeTrigger = modal.querySelector(".modal__close");
 
+	const mobileTrigger = document.querySelector(".menu-icon-wrapper");
+	const mobileBody = document.querySelector(".menu-adaptive");
+
+	const mobileMenuLinks = document.querySelectorAll(".menu__link");
+
+	mobileMenuLinks.forEach(el => {
+		el.addEventListener("click", () => {
+			mobileBody.classList.remove("active");
+			backdrop.classList.remove("active");
+		});
+	});
+
+	backdrop.addEventListener("click", () => {
+		mobileBody.classList.remove("active");
+		backdrop.classList.remove("active")
+	});
+
 	select();
 	modalInit(modal, backdrop, modalTriggers, closeTrigger);
+	mobileMenu(mobileTrigger, mobileBody, backdrop);
+
+	AOS.init();
 });
+
+function mobileMenu(mobileTrigger, mobileBody, backdrop) {
+	mobileTrigger.addEventListener("click", function() {
+		if (!mobileBody.classList.contains("active")) {
+			mobileBody.classList.add("active");
+			backdrop.classList.add("active");
+		} else {
+			mobileBody.classList.remove("active");
+			backdrop.classList.remove("active");
+		}
+	});
+}
 
 function modalInit(modal, backdrop, modalTriggers, closeTrigger) {
 	modalTriggers.forEach(trigger => {
